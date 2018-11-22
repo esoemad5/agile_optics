d3048f2b4376b7308daf6bb63f9d31423cea5a54
esoemad5 <esoemad5@gmail.com>
2018-11-22 12:22:40 -0600

    Users can type in the UpdateTask form, but submit does nothing.

--- src/components/UpdateTask.js
+++ src/components/UpdateTask.js
@@ -8,7 +8,6 @@ class UpdateTask extends React.Component{
         e.preventDefault();
         console.clear();
         console.log(this.state);
-        console.log(this.props);
     }
     handleChange = (e) => {
         this.setState({
@@ -16,24 +15,37 @@ class UpdateTask extends React.Component{
         })
     }
     render() {
-        console.log(this.props);
+        // console.clear();
+        // console.log(this.state);
+        // console.log(this.props);
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit}>
-                    <button className="ui button" onClick={this.handleSubmit}>Submit</button>
-                    <h1 className="ui segment center aligned"><input type='text' id='name' value={this.props.task.name} onChange={this.handleChange} /></h1>
+                    <button
+                        className="ui button"
+                        onClick={this.handleSubmit}>
+                        Submit
+                    </button>
+                    <h1 className="ui segment center aligned">
+                        <input
+                            type='text'
+                            id='name'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </h1>
                     <i><b>
                         Status: <input
                             type='text'
                             id='status'
-                            value={this.props.task.status}
+                            value={this.state.status}
                             onChange={this.handleChange}
                         />
                         <br />
                         Priority: <input
                             type='text'
                             id='priority'
-                            value={this.props.task.priority}
+                            value={this.state.priority}
                             onChange={this.handleChange}
                         />
                     </b></i>
