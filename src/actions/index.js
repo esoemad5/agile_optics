21a916dcdbdc8f6b7e7e8b4c090a6da386a01fc9
esoemad5 <esoemad5@gmail.com>
2018-11-24 17:31:49 -0600

    Creating tasks works nearly perfectly.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,8 +7,31 @@ export const selectTask = (task) => {
 };
 
 export const createTaskOnModule = (task, moduleId) => {
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
-        // make async call to database
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        /*
+         * 0: Stuck; 1: In Progress; 2: Awaiting Review; 3: Not Started; 4: Completed
+         */
+        switch (task.status) {
+            case "Stuck":
+                task.status = 0;
+                break;
+            case "In Progress":
+                task.status = 1;
+                break;
+            case "Awaiting Review":
+                task.status = 2;
+                break;
+            case "Not Started":
+                task.status = 3;
+                break;
+            case "Completed":
+                task.status = 4;
+                break;
+            default:
+                task.status = -1;
+                break;
+            
+        }
         const firestore = getFirestore(); // this is a refference to the database
         firestore.collection('Tasks')
         .add( { ...task, moduleId: moduleId } )
