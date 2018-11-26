3b1508e3adb6410f90e013843edcc3dc95efc037
esoemad5 <esoemad5@gmail.com>
2018-11-26 15:51:08 -0600

    renamed comments to commentsSection

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Comments extends React.Component {
+class CommentSection extends React.Component {
     render() {
         return (
             <div className="ui raised very padded text container segment fluid">
