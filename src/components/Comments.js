c1ce88b43e63b96e19e508b3663a91eb77c6bf55
esoemad5 <esoemad5@gmail.com>
2018-11-26 21:32:06 -0600

    Outlined comments section.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
 class Comments extends React.Component {
+    state = {
+        commentEditor: "",
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        this.setState({
+            commentEditor: "",
+        })
+    }
+    handleChange = (e) => {
+        this.setState({
+            commentEditor: e.target.value
+        })
+    }
+
+    writeNewCommentForm = () => {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <textarea
+                    type='text'
+                    id='commentEditor'
+                    value={this.state.commentEditor}
+                    onChange={this.handleChange}
+                />
+                <br />
+                <button
+                    className="ui button"
+                    onClick={this.handleSubmit}>
+                    Submit
+                    </button>
+            </form>
+        );
+    }
     render() {
         return (
             <div className="ui raised very padded text container segment fluid">
-                Comments
+                <p>render comments</p>
+                {this.writeNewCommentForm()}
             </div>
         );
     }
